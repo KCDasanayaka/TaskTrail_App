8e4f3d6b8ec1ddb66fc24f9052cce21c6029ce78
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-10-07 01:05:12 +0530

    created calender page

--- todo/app/(tabs)/explore.tsx
+++ todo/app/(tabs)/explore.tsx
@@ -1,31 +1,49 @@
+import React from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import { View, Text, StyleSheet, Image, Platform, TouchableOpacity, SafeAreaView } from 'react-native';
+import {Agenda} from 'react-native-calendars';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
-export default function TabTwoScreen() {
+export default function calendars() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.container}>
+      <Agenda
+        items={{
+          '2024-03-26': [{ name: 'Meeting 1', date: 'Event details...' }],
+          '2024-03-28': [{ name: 'Meeting 2', date: 'Event details...' }],
+          '2024-03-29': [{ name: 'Meeting 3', date: 'Event details...' }],
+        }}
+        selected={'2024-03-26'} // This will ensure the initial date is correct
+        renderItem={(item, isFirst) => (
+          <TouchableOpacity style={styles.item}>
+            <Text style={styles.itemTitle}>{item.name}</Text>
+            <Text style={styles.itemDescription}>{item.date}</Text>
+          </TouchableOpacity>
+        )}
+      />
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
   },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  item: {
+    backgroundColor: 'white',
+    padding: 20,
+    marginVertical: 10,
+    borderRadius: 10,
+  },
+  itemTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  itemDescription: {
+    fontSize: 14,
+    color: '#888',
   },
 });
+
